b5ce808a6b1cdb3f1886d59b8e8dfa3230792315
CodersAcademy006 <104912634+CodersAcademy006@users.noreply.github.com>
2024-08-30 11:06:05 -0700

    Add: Concatenate Two Strings.py

---
+++ String Based/Concatenate Two Strings.py
@@ -0,0 +1,7 @@
+def concatenate_strings(s1, s2):
+    return s1 + s2
+
+# Take input from user
+s1 = input("Enter the first string: ")
+s2 = input("Enter the second string: ")
+print(f"Concatenated string: {concatenate_strings(s1, s2)}")
